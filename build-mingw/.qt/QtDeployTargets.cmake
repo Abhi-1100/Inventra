420e9075fd8d687879e716964348b82c1eca3d40
omghori18 <omghori3528@gmail.com>
2026-08-03 20:42:06 +0530

    Clean up build artifacts and pycache from repository tracking

--- build-mingw/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Inventra_FILE D:/confres/build-mingw/Inventra.exe)
-set(__QT_DEPLOY_TARGET_Inventra_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Inventra_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
--- build-mingw/CMakeFiles/Inventra.dir/Inventra_autogen/3YJK5W5UP7/qrc_inventra.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Inventra.dir/Inventra_autogen/3YJK5W5UP7/qrc_inventra.cpp.obj: \
- D:\confres\build-mingw\Inventra_autogen\3YJK5W5UP7\qrc_inventra.cpp
